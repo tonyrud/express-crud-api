64edf3c68e73d9547b2e87d60e7fa852e68c90ce
tonyrud <tonrudny@gmail.com>
2017-08-05 07:28:44 -0400

    added delete todos

--- server/server.js
+++ server/server.js
@@ -55,6 +55,25 @@ app.get('/todos/:id', (req, res) => {
   )
 })
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then(
+    todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({ todo })
+    },
+    e => {
+      res.status(400).send(e)
+    }
+  )
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
