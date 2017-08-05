d0043dc773b6f1b49ef572635aa0657db98e13de
tonyrud <tonrudny@gmail.com>
2017-08-05 07:58:25 -0400

    add patch updating to api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const {ObjectId} = require('mongodb')
+const { ObjectId } = require('mongodb')
+const _ = require('lodash')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -72,6 +73,39 @@ app.delete('/todos/:id', (req, res) => {
       res.status(400).send(e)
     }
   )
+})
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id
+  const body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    { new: true }
+  )
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send({todo})
+    })
+    .catch(err => {
+      res.status(404).send()
+    })
 })
 
 app.listen(port, () => {
